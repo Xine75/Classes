0f1844e3d502052623b01fe87f570e3d0adfbeab
Xine <cjdoza@gmail.com>
2021-03-03 16:25:01 -0600

    initial

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Classes
+{
+    public class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public string Title { get; }
+
+        public DateTime StartDate { get; }
+    }
+
+}
+
